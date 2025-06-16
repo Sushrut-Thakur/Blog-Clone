ae87f3b9ba4cc45aa389df0f58f8d85e7fdcd7b3
Sushrut-Thakur <sushrut21thakur@gmail.com>
2025-06-16 15:31:35 +0530

    Customised admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from blog.models import Post, Comment
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+	fields = ['author', 'title', 'text', 'published_date']
+	search_fields = ['title']
+	list_filter = ['published_date', 'author']
+	list_display = ['title', 'author']
+
+class CommentAdmin(admin.ModelAdmin):
+	fields = ['post', 'author', 'text', 'approved']
+	list_display = ['comment', 'author', 'post', 'approved']
+	list_editable = ['approved']
+
+	def comment(self, obj):
+		return (obj.text[:30] + '...') if len(obj.text) > 30 else obj.text
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
